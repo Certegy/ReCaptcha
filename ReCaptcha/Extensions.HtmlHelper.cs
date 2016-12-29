1d9b11ff6f3587a231460e135d6be92146f9be77
Ryan Bartsch <rbartsch@homestart.com.au>
2016-12-29 22:17:27 +1030

    refactored HtmlHelperExtensions

--- ReCaptcha/Extensions.HtmlHelper.cs
+++ ReCaptcha/Extensions.HtmlHelper.cs
@@ -1,16 +1,19 @@
 using System;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace ReCaptcha
 {
     public static class HtmlHelperExtensions
     {
+        private const string RecaptchaCreateScript = @"Recaptcha.create(""{0}"", ""recaptcha_div"", {{ theme: ""{1}"" {2}}})";
+
         public static MvcHtmlString GenerateCaptcha(this HtmlHelper value, Theme theme, string callBack = null)
         {
-            const string htmlInjectString = @"<div id=""recaptcha_div""></div>
-                <script type=""text/javascript"">
-                    Recaptcha.create(""{0}"", ""recaptcha_div"", {{ theme: ""{1}"" {2}}});
-                </script>";
+            var div = new XElement("div", new XAttribute("id", "recaptcha_div"));
+            var script = new XElement("script", new XAttribute("type", "text/javascript"), RecaptchaCreateScript);
+
+            var htmlInjectString = $"{div}{Environment.NewLine}{script}";
             
             var publicKey = ReCaptchaResourceSettingsLocator.Get(settings => settings.ReCaptchaSiteKey);
 
